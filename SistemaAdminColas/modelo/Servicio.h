c71ee5c5fe2ecfe4a6ee7d091c58109de72e1d61
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-05-09 03:39:56 -0600

    add Servicio class declaration

--- SistemaAdminColas/modelo/Servicio.h
+++ SistemaAdminColas/modelo/Servicio.h
@@ -1,4 +1,37 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+
 class Servicio {
+private:
+	std::string nombre;
+	std::string descripcion;
+	int prioridad;
+	std::string codigoArea;
+	int codigoServicio;
+	int cantidadTiquetes;
+
+public:
+	Servicio(const std::string& nombre, const std::string& descripcion, int prioridad, int codigoArea);
+	Servicio(const Servicio&) = default;
+	Servicio& operator=(const Servicio&) = default;
+	~Servicio() = default;
+
+	std::string getNombre() const;
+	void setNombre(const std::string& nombre);
+
+	std::string getDescripcion() const;
+	void setDescripcion(const std::string& descripcion);
+
+	int getPrioridad() const;
+	void setPrioridad(int prioridad);
+
+	int getArea() const;
+
+	void aumentarTiquetes();
+	int getTotalTiquetes() const;
+
+	std::string toString() const;
+	friend std::ostream& operator<<(std::ostream& os, const Servicio& servicio);
 };
