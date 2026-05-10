bd4d94bfde982f2f7abf40d7b969b2e16d7578a5
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-05-10 03:17:08 -0600

    implement Sistema constructor and destructor

--- SistemaAdminColas/sistema/Sistema.cpp
+++ SistemaAdminColas/sistema/Sistema.cpp
@@ -1,5 +1,29 @@
+#include <iostream>
 #include "Sistema.h"
+#include "../ed/LinkedList.h"
+#include "../modelo/TipoUsuario.h"
+
+using std::string;
 
 Sistema::Sistema() {
+	usuarios = new LinkedList<TipoUsuario>();
+	servicios = new LinkedList<Servicio>();
+	// areas = new LinkedList<Area*>();		pendiente
+}
 
+Sistema::~Sistema() {
+	delete usuarios;
+	delete servicios;
+	// delete areas;
+}
+
+// --- Tipos de usuario ---
+void Sistema::agregarTipoUsuario(const std::string& descripcion, int prioridad) {
+	TipoUsuario nuevo(descripcion, prioridad);
+
+	usuarios->goToStart();
+	while (!usuarios->atEnd() && usuarios->getElement() < nuevo) {
+		usuarios->next();
+	}
+	usuarios->insert(nuevo);
 }
