6864b660d4ae197ad37b1ca11abfe025f278fcf8
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-05-09 21:42:23 -0600

    configure locale and UTF-8 console support

--- SistemaAdminColas/interfaz/Interfaz.cpp
+++ SistemaAdminColas/interfaz/Interfaz.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include "Interfaz.h"
 #include "../sistema/Sistema.h"
 
--- SistemaAdminColas/main.cpp
+++ SistemaAdminColas/main.cpp
@@ -1,7 +1,12 @@
+﻿#include <clocale>
+#include <Windows.h>
 #include "sistema/Sistema.h"
 #include "interfaz/Interfaz.h"
 
 int main() {
+	SetConsoleOutputCP(CP_UTF8);
+	setlocale(LC_ALL, "");
+
 	Sistema sistema;
 	Interfaz interfaz(sistema);
 	interfaz.iniciar();
