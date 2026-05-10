e1edbd8d70d301eae5d555ce2e683c7ac693bffe
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-05-09 19:36:15 -0600

    add Ventanilla class declaration

--- SistemaAdminColas/modelo/Ventanilla.h
+++ SistemaAdminColas/modelo/Ventanilla.h
@@ -1,4 +1,31 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+#include "Tiquete.h"
+
 class Ventanilla {
+private:
+	std::string nombre;
+	int tiquetesAtendidos;
+	Tiquete tiqueteActual;
+	bool tieneTiqueteActual;
+
+public:
+	Ventanilla() = default;
+	Ventanilla(const std::string& nombre);
+	Ventanilla(const Ventanilla&) = default;
+	Ventanilla& operator=(const Ventanilla&) = default;
+	~Ventanilla() = default;
+
+	Tiquete getTiquete() const;
+	void atenderTiquete();
+	int getTiquetesAtendidos() const;
+
+	std::string getNombre() const;
+
+	bool getTieneTiqueteActual() const;
+
+	std::string toString() const;
+	friend std::ostream& operator<<(std::ostream& os, const Ventanilla& ventanilla);
 };
