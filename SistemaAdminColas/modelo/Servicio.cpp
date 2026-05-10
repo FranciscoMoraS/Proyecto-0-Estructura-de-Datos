06057fa3680ce3520efa1ef7843a56957fd14522
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-05-10 02:17:13 -0600

    implement Servicio class

--- SistemaAdminColas/modelo/Servicio.cpp
+++ SistemaAdminColas/modelo/Servicio.cpp
@@ -1,1 +1,36 @@
+﻿#include <iostream>
+#include <string>
 #include "Servicio.h"
+
+using std::string;
+
+Servicio::Servicio(const string& descripcion, int prioridad, const string& codigoArea) {
+	this->descripcion = descripcion;
+	this->prioridad = prioridad;
+	this->codigoArea = codigoArea;
+}
+
+int Servicio::getPrioridad() const {
+	return prioridad;
+}
+
+const string& Servicio::getCodigoArea() const {
+	return codigoArea;
+}
+
+void Servicio::aumentarTiquetes() {
+	totalTiquetes++;
+}
+
+int Servicio::getTotalTiquetes() const {
+	return totalTiquetes;
+}
+
+string Servicio::toString() const {
+	return descripcion + " [área: " + codigoArea + ", prioridad: " + std::to_string(prioridad) + "]";
+}
+
+std::ostream& operator<<(std::ostream& os, const Servicio& servicio) {
+	os << servicio.toString();
+	return os;
+}
