c65be3e49722fda92fea38462f51cf080d4d223d
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-05-09 22:15:45 -0600

    implement Intefaz menuPrincipal

--- SistemaAdminColas/interfaz/Interfaz.cpp
+++ SistemaAdminColas/interfaz/Interfaz.cpp
@@ -8,13 +8,27 @@ Interfaz::Interfaz(Sistema& sistema) : sistema(sistema), ejecutando(true) {}
 
 void Interfaz::iniciar() {
 	while (ejecutando) {
-		cout << "Inicio programa\n";
+		menuPrincipal();
 	}
 }
 
 // ===================================================================
 // MENÚS
 // ===================================================================
+void Interfaz::menuPrincipal() {
+	limpiarPantalla();
+	cout << "===== SISTEMA DE ADMINISTRACIÓN DE COLAS =====\n";
+	cout << "1. Estado de las colas\n";
+	cout << "2. Tiquetes\n";
+	cout << "3. Atender\n";
+	cout << "4. Administración\n";
+	cout << "5. Estadísticas del sistema\n";
+	cout << "6. Salir\n";
+
+	//int opcion = leerEnteroEnRango("Seleccione una opción: ", 1, 6);
+	//switch
+	ejecutando = false;
+}
 
 void Interfaz::limpiarPantalla() {
 	std::system("cls");
