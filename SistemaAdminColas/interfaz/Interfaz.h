c158f1e08127ace07353071cdc02091f0c7103b6
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-05-09 21:52:54 -0600

    add declarations for interface menus

--- SistemaAdminColas/interfaz/Interfaz.h
+++ SistemaAdminColas/interfaz/Interfaz.h
@@ -9,6 +9,14 @@ private:
 	Sistema& sistema;
 	bool ejecutando;
 
+	// --- Menús (loops de navegación) ---
+	void menuPrincipal();
+	void menuTiquetes();
+	void menuAdministracion();
+	void menuTipoUsuario();
+	void menuAreas();
+	void menuServicio();
+
 public:
 	Interfaz(Sistema& sistema);
 	void iniciar();
