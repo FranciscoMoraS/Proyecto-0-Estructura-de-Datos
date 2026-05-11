416305d9621f40726f3af70b189a141d194d672f
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-05-10 23:02:55 -0600

    add Util documentation

--- SistemaAdminColas/ed/Util.h
+++ SistemaAdminColas/ed/Util.h
@@ -1,3 +1,12 @@
+/*
+ * Archivo: Util.h
+ * Autores: Edwin Muñoz y Francisco Mora
+ *
+ * Expone DEFAULT_MAX, que define la capacidad por defecto 
+ * del MinHeap subyacente al HeapPriorityQueue.
+ *
+ */
+
 #pragma once
 
 constexpr auto DEFAULT_MAX = 1024;
